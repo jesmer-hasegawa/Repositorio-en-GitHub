f5884a919e7e2e4199217a8d7ab77cc3220dafd5
Homero <111809316+jesmer-hasegawa@users.noreply.github.com>
2022-11-29 20:35:14 -0600

    busca títulos con información más precisa

---
+++ 6. Webscraping/scrap4.py
@@ -0,0 +1,26 @@
+# Importacion de modulos
+import requests
+from bs4 import BeautifulSoup
+# Jesus Homero Galindo Gaytan
+
+# Obtencion de los datos mediante peticién GET
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get (URL)
+
+# Analizamos el contenido HTML con Beautifulsoup
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="ResultsContainer")
+
+# Buscar todos los elementos que el class "card-content”
+job_elements = results.find_all("div", class_="card-content")
+
+# En el objeto job_elemento buscamos solo aquellos elementos con
+# titulo e informacién relevante.
+for job_element in job_elements:
+	title_element = job_element.find("h2", class_="title")
+	company_element = job_element.find("h3", class_="company")
+	location_element = job_element.find("p", class_="location")
+	print (title_element)
+	print (company_element)
+	print (location_element)
+	print ()
