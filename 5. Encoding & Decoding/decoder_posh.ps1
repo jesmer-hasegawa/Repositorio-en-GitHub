b219fc6ded0d38105f06b30f2ee71409d5c242ed
Homero <111809316+jesmer-hasegawa@users.noreply.github.com>
2022-11-29 20:26:29 -0600

    decodificar una cadena en Base64

---
+++ 5. Encoding & Decoding/decoder_posh.ps1
@@ -0,0 +1,18 @@
+﻿# Limpiando pantalla
+Clear-Host
+#
+# Mensaje de bienvenido
+#
+Write-Host "Ejemplo de Decodificador Base64 en PowerShell” -ForegroundColor Yellow
+#
+# Mensaje codificado en Base64
+#
+$texto = 'TABhAGIAbwByAGEAdABvAHIAaQBvACAAZABlACAAUAByAG8AZwByAGEAbQBhAGMAaQDzAG4AIABwAGEAcgBhACAAQwBpAGIAZQByAFMAZQBnAHUAcgBpAGQAYQBkACAAUwBlAHMAaQDzAG4AIAAxADAA'
+Write-Host " La cadena a decodificar es:”
+Write-Host $texto
+#
+# Decodificando mensaje
+#
+$decod = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($texto))
+Write-Host "La cadena ya decodificada es:" -ForegroundColor Green
+Write-Host $decod
