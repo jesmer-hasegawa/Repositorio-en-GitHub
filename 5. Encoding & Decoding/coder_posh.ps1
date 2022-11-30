acd0446e25a523f6f8cfa17ceb00a3f6754884e1
Homero <111809316+jesmer-hasegawa@users.noreply.github.com>
2022-11-29 20:26:00 -0600

    codificar en Base64

---
+++ 5. Encoding & Decoding/coder_posh.ps1
@@ -0,0 +1,12 @@
+﻿# Limpiando pantalla
+Clear-Host
+# Mensaje de bienvenida
+Write-Host "Ejemplo de codificador Base64 en PowerShell" -ForegroundColor Yellow
+Write-Host "Escribe una frase a codificar: "-ForegroundColor Yellow
+# Solicitando la entrada de una cadena de texto.
+$frase = Read-Host
+#Codificando en Base64 y guardando resultado en una cadena.
+$encod = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes(($frase)))
+#Imprimiendo la salida
+Write-Host "La frase escrita en Base 64 es: " -Foregroundcolor Green
+Write-Output $encod
