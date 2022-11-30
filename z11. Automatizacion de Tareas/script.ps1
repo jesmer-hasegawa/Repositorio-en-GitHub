d4d4c8a64c44d75bb2cd969c26c2b1182f0ae9ee
Homero <111809316+jesmer-hasegawa@users.noreply.github.com>
2022-11-29 21:12:30 -0600

    automatizar actividades con powershell

---
+++ z11. Automatizacion de Tareas/script.ps1
@@ -0,0 +1,5 @@
+﻿# Jesus Homero Galindo Gaytan
+$envio = New-ScheduledTaskAction -Execute 'send_sysinfo.ps1'
+$trigger = New-ScheduledTaskTrigger -Once -At 11:21am
+Register-ScheduledTask -Action $envio -Trigger $trigger -TaskPath "Mis Tareas" -TaskName "Envio correo" -Description "Envio informacion a un correo"
+
